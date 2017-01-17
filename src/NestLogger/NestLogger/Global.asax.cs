57df367a0c5ca0034337a7a68f7a8334882c0fd5
John Battye <john.battye@sky.uk>
2017-01-17 22:33:26 +0000

    Change culture to gb for dates and formatted some doubles

--- src/NestLogger/NestLogger/Global.asax.cs
+++ src/NestLogger/NestLogger/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -18,6 +20,11 @@ namespace NestLogger
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
         }
     }
 }
